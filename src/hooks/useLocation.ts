50d69c322ffd303e962eed04e114d220aed406c8
takeyama <keigo@virtual-tech.net>
2025-12-09 13:08:52 +0900

    ログアウト時の遷移先修正

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -4,12 +4,11 @@ import React from 'react'
 export const locationStorageAtom = atomWithStorage('location', '')
 
 const useLocation = () => {
-
   const [service_name] = React.useState(location.host.replace('.vte.cx', ''))
 
   const moveLogin = React.useCallback(() => {
     if (window.location.pathname.indexOf('admin.html') !== -1) {
-      window.location.href = `login.html?service_name=${service_name}`
+      window.location.href = `https://admin.vte.cx/login.html?service_name=${service_name}`
     } else {
       window.location.href = 'login.html'
     }
